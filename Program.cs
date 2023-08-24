13df4f87ea7cfef6df23e1ec357ac57baa862ad6
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-08-24 12:29:12 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, TE22!");
+
+string name = "Micke";
+
+Console.WriteLine("Hej " + name + "! Vad cool du är idag!");
+Console.WriteLine($"Hej då {name}! Vi ses imorgon!");
+
+Console.ReadLine();
